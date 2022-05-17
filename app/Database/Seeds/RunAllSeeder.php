641cf228b342f1d90e86f372476cb2b4c4f1af25
Otoniel Flores <55592246+abdieloflores@users.noreply.github.com>
2022-05-17 13:58:50 -0700

    Sedeer para correr todos los seeders

---
+++ app/Database/Seeds/RunAllSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class RunAllSeeder extends Seeder
+{
+	public function run()
+	{
+		$this->call('EstadosSeeder');
+		$this->call('MunicipioSeeder');
+		$this->call('LocalidadSeeder');
+		$this->call('ColoniaSeeder');
+		$this->call('OficinaSeeder');
+		$this->call('AreaSeeder');
+		$this->call('ClasificacionDoctoSeeder');
+		$this->call('PlantillaSeeder');
+		$this->call('DelitoModalidadSeeder');
+		$this->call('MedioConocimientoSeeder');
+		$this->call('PerfilesSeeder');
+		$this->call('PersonaCalidadJuridicaSeeder');
+		$this->call('PersonaEdoCivilSeeder ');
+		$this->call('PersonaIdiomaSeeder  ');
+		$this->call('PersonaNacionalidadSeeder');
+		$this->call('PersonaReligionSeeder');
+		$this->call('PersonaTipoIdentificacionSeeder');
+		$this->call('TipoExpedienteSeeder  ');
+		$this->call('TipoViviendaSeeder ');
+		$this->call('VehiculoColorSeeder');
+		$this->call('VehiculoTipoSeeder ');
+		$this->call('UsersSeeder');
+	}
+}
