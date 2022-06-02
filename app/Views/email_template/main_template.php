5406271e0ad76de45a9ea824f4e27ba8031d4af7
Otoniel Flores <55592246+abdieloflores@users.noreply.github.com>
2022-06-02 16:51:43 -0700

    Ajuste plantilla de correos

---
+++ app/Views/email_template/main_template.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<style type="text/css">
+		/* CLIENT-SPECIFIC STYLES */
+		body,
+		table,
+		td,
+		a {
+			-webkit-text-size-adjust: 100%;
+			-ms-text-size-adjust: 100%;
+		}
+
+		table,
+		td {
+			mso-table-lspace: 0pt;
+			mso-table-rspace: 0pt;
+		}
+
+		img {
+			-ms-interpolation-mode: bicubic;
+		}
+
+		/* RESET STYLES */
+		img {
+			border: 0;
+			height: auto;
+			line-height: 100%;
+			outline: none;
+			text-decoration: none;
+		}
+
+		table {
+			border-collapse: collapse !important;
+		}
+
+		body {
+			height: 100% !important;
+			margin: 0 !important;
+			padding: 0 !important;
+			width: 100% !important;
+			font-family: Helvetica, Arial, sans-serif !important;
+			font-size: 16px;
+		}
+
+		h1,
+		h2,
+		h3,
+		h4,
+		h5,
+		h6,
+		p {
+			margin-top: 0px !important;
+			margin-bottom: 10px;
+		}
+
+		h1,
+		h2,
+		h3,
+		h4,
+		h5,
+		h6 {
+			color: #092b47 !important;
+		}
+
+		/* iOS BLUE LINKS */
+		a[x-apple-data-detectors] {
+			/* color: inherit !important; */
+			text-decoration: none !important;
+			font-size: inherit !important;
+			font-family: inherit !important;
+			font-weight: inherit !important;
+			line-height: inherit !important;
+			color: #bf9b55 !important;
+		}
+
+		/* ANDROID CENTER FIX */
+		div[style*="margin: 16px 0;"] {
+			margin: 0 !important;
+		}
+
+		body {
+			font-size: 15px !important;
+		}
+
+		.body {
+			background-color: #e5e4e3 !important;
+			padding: 20px 50px;
+		}
+
+		.body .btn {
+			padding: 15px 10px;
+			border: none;
+			color: #ffffff;
+			background-color: #bf9b55;
+			font-weight: bold;
+			border-radius: 5px;
+			cursor: pointer;
+			text-decoration: none;
+			font-size: 14px;
+			margin: 15px;
+		}
+
+		.body .btn:hover {
+			background-color: #a18347;
+		}
+
+		.footer {
+			background-color: #e5e4e3 !important;
+			padding: 20px;
+			text-align: center;
+		}
+
+		.footer p {
+			font-size: 0.8em !important;
+			margin: 0px;
+		}
+	</style>
+</head>
+
+<body style="margin: 0 !important; padding: 0 !important; background-color: #7D7D7D!important;" bgcolor="#7D7D7D">
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">
+		<tr>
+			<td align="center" style="background-color: #7D7D7D!important;padding-top: 50px;padding-bottom: 50px"
+				bgcolor="#7D7D7D">
+				<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+					<tr>
+						<td border="0" valign="top"
+							style="font-size: 0; background-color: #511229!important;height: 294px;background-image: url('https://yocontigo-it.com.mx/justicia/public/assets/img/email/HEADER_CORREO.jpg');background-repeat: no-repeat; background-size: 600px 294px;"
+							bgcolor="#511229"></td>
+					</tr>
+					<tr>
+						<td style="padding: 0px 50px 50px 50px; background-color: #ffffff!important;" bgcolor="#ffffff">
+							<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:500px;">
+								<tr>
+									<td class="body" bgcolor="#E5E4E3">
+										<?= $this->renderSection('body') ?>
+									</td>
+								</tr>
+								<tr>
+									<td class="footer" bgcolor="#E5E4E3">
+										<p>
+											Si usted no ha realizado esta
+											acción ignore este mensaje.
+										</p>
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+</body>
+
+</html>
--- app/Views/email_template/password_email_template.php
+++ app/Views/email_template/password_email_template.php
@@ -1,155 +1,19 @@
-<!DOCTYPE html>
-<html>
+<?= $this->extend('email_template/main_template') ?>
+<?= $this->section('body') ?>
 
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<style type="text/css">
-		/* CLIENT-SPECIFIC STYLES */
-		body,
-		table,
-		td,
-		a {
-			-webkit-text-size-adjust: 100%;
-			-ms-text-size-adjust: 100%;
-		}
+<div style="text-align:center;">
+	<p>
+		Usted ha generado un nuevo registro en el Centro de Denuncia Tecnológica.
+		<br>Para acceder debes ingresar los siguientes datos
+	</p>
+	<p style="font-weight: bold;">
+		USUARIO: <?= $email ?><br>
+		CONTRASEÑA: <?= $password ?>
+	</p>
+	<br><br>
+	<a class="btn" href="<?= base_url('/denuncia') ?>">
+		INICIAR VIDEO DENUNCIA
+	</a>
+</div>
 
-		table,
-		td {
-			mso-table-lspace: 0pt;
-			mso-table-rspace: 0pt;
-		}
-
-		img {
-			-ms-interpolation-mode: bicubic;
-		}
-
-		/* RESET STYLES */
-		img {
-			border: 0;
-			height: auto;
-			line-height: 100%;
-			outline: none;
-			text-decoration: none;
-		}
-
-		table {
-			border-collapse: collapse !important;
-		}
-
-		body {
-			height: 100% !important;
-			margin: 0 !important;
-			padding: 0 !important;
-			width: 100% !important;
-			font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif !important;
-			font-size: 16px;
-		}
-
-		h1,
-		h2,
-		h3,
-		h4,
-		h5,
-		h6,
-		p {
-			margin-top: 0 !important;
-		}
-
-		h1,
-		h2,
-		h3,
-		h4,
-		h5,
-		h6 {
-			color: #092b47 !important;
-		}
-
-
-		/* iOS BLUE LINKS */
-		a[x-apple-data-detectors] {
-			/* color: inherit !important; */
-			text-decoration: none !important;
-			font-size: inherit !important;
-			font-family: inherit !important;
-			font-weight: inherit !important;
-			line-height: inherit !important;
-			color: #bf9b55 !important;
-		}
-
-		/* ANDROID CENTER FIX */
-		div[style*="margin: 16px 0;"] {
-			margin: 0 !important;
-		}
-
-		.body {
-			margin: 50px 0px 50px 0px;
-		}
-
-		.body .btn {
-			padding: 15px 10px;
-			border: none;
-			color: #ffffff;
-			background-color: #bf9b55;
-			font-weight: bold;
-			border-radius: 5px;
-			cursor: pointer;
-			text-decoration: none;
-			font-size: 14px;
-		}
-
-		.body .btn:hover {
-			background-color: #a18347;
-		}
-
-		.footer p {
-			font-size: .8em !important;
-		}
-	</style>
-</head>
-
-<body style="margin: 0 !important; padding: 0 !important; background-color: #7D7D7D!important;" bgcolor="#7D7D7D">
-	<table border="0" cellpadding="0" cellspacing="0" width="100%">
-		<tr>
-			<td align="center" style="background-color: #7D7D7D!important;padding-top: 50px;padding-bottom: 50px" bgcolor="#7D7D7D">
-				<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
-					<tr>
-						<td valign="top" style="font-size: 0; background-color: #511229!important;height: 200px;background-image: url('https://yocontigo-it.com.mx/justicia/public/assets/img/email/LINEAS_CABECERA_CORREO.png ');background-repeat: no-repeat;
-							background-size: 600px 200px;" bgcolor="#511229">
-						</td>
-					</tr>
-					<tr>
-						<td style="padding: 0px 50px 50px 50px; background-color: #ffffff!important;" bgcolor="#ffffff">
-							<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
-								<td style="background-color: #E5E4E3!important;" bgcolor="#E5E4E3">
-									<div style="background-color: #E5E4E3!important; margin-top: -100px;padding: 0px 20px 0px 20px;text-align: center;">
-										<img src="https://yocontigo-it.com.mx/justicia/public/assets/img/email/LOGO_CORREO.png" alt="Logo" style="margin-top: -100px;width: 200px;">
-										<div class="body">
-											<p>
-												Usted ha generado un nuevo registro en el Centro de Denuncia Tecnológica.
-												<br>Para acceder debes ingresar los siguientes datos
-											</p>
-											<p>
-												USUARIO: <?= $email ?><br>
-												CONTRASEÑA: <?= $password ?>
-											</p>
-											<a class="btn" href="<?= base_url('/denuncia') ?>">
-												INICIAR VIDEO DENUNCIA
-											</a>
-										</div>
-										<div class="footer">
-											<p>Si usted no ha realizado esta acción ignore este mensaje.</p>
-										</div>
-									</div>
-								</td>
-							</table>
-						</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-	</table>
-</body>
-
-</html>
+<?= $this->endSection() ?>
