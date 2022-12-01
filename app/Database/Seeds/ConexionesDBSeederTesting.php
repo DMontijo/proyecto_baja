def51653ae7d7172e81a164008e46fc9bf1081d3
Otoniel Flores <55592246+abdieloflores@users.noreply.github.com>
2022-12-01 11:31:22 -0800

    Update seeder

--- app/Database/Seeds/ConexionesDBSeederTesting.php
+++ app/Database/Seeds/ConexionesDBSeederTesting.php
@@ -4,7 +4,7 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 
-class ConexionesDBSeeder extends Seeder
+class ConexionesDBSeederTesting extends Seeder
 {
 	public function run()
 	{
