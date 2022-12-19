87d6ff0b34a890f4dd83702574c7ed2352a1bc58
zeika-coder <andygtz99@gmail.com>
2022-12-19 16:23:12 -0600

    update form denuncia

--- app/Views/admin/dashboard/video_denuncia_forms/form_denuncia.php
+++ app/Views/admin/dashboard/video_denuncia_forms/form_denuncia.php
@@ -4,7 +4,7 @@
 	</div>
 	<div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-3">
 		<label for="delito_delito" class="form-label font-weight-bold">Delito:</label>
-		<select class="form-control" id="delito_delito" name="delito_delito" required>
+		<select class="form-control" id="delito_delito" name="delito_delito">
 			<option selected disabled value="">Selecciona el delito</option>
 			<?php foreach ($body_data->delitosUsuarios as $index => $delitos) { ?>
 				<option value="<?= $delitos->DELITO ?>"> <?= $delitos->DELITO ?> </option>
