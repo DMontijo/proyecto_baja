8b1a95cedf864931000a566f04ea6598b422b50e
Otoniel Flores <55592246+abdieloflores@users.noreply.github.com>
2023-01-12 15:52:37 -0800

    Fix - Ajuste de email constancias y perfil

--- app/Views/email_template/constancia_firmada_email_template.php
+++ app/Views/email_template/constancia_firmada_email_template.php
@@ -2,15 +2,22 @@
 <?= $this->section('body') ?>
 
 <div style="text-align:center;">
-	<h2>SE HA FIRMADO TU CONSTANCIA:</h2>
-	<p>Para darle seguimiento a su constancia de extravio ingrese a su cuenta en el Centro de Denuncia Tecnológica e inicie sesión para visualizarla.</p>
-
-	<br>
-	<br>
-	<br>
-	<p style="font-size:10px;">
-		<a href="<?= base_url() ?>/assets/documentos/Aviso_De_Privacidad_De_Datos.pdf">Aviso de privacidad</a> | <a href="<?= base_url() ?>/assets/documentos/TerminosCondiciones.pdf">Términos y condiciones</a> | <a href="<?= base_url() ?>/assets/documentos/DerechosDeVictimaOfendido.pdf">Derechos de víctima u ofendido.</a>
-	</p>
+    <h2>SE HA FIRMADO TU CONSTANCIA SOLICITADA:</h2>
+    <p>Ingrese a su cuenta en el Centro de Denuncia Tecnológica e inicie sesión para visualizarla descargarla
+        nuevamente.</p>
+    <br>
+    <br>
+    <a class="btn" href="<?= base_url('/constancia_extravio') ?>">
+        IR A PLATAFORMA
+    </a>
+    <br>
+    <br>
+    <br>
+    <p style="font-size:10px;">
+        <a href="<?= base_url() ?>/assets/documentos/Aviso_De_Privacidad_De_Datos.pdf">Aviso de privacidad</a> | <a
+            href="<?= base_url() ?>/assets/documentos/TerminosCondiciones.pdf">Términos y condiciones</a> | <a
+            href="<?= base_url() ?>/assets/documentos/DerechosDeVictimaOfendido.pdf">Derechos de víctima u ofendido.</a>
+    </p>
 </div>
 
 <?= $this->endSection() ?>
