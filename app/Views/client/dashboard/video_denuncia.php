48179881e56f140d3d14f758fd8fcaf240e7ae6b
zeika-coder <andygtz99@gmail.com>
2023-03-22 21:10:45 -0600

    interfaz video denuncia

--- app/Views/client/dashboard/video_denuncia.php
+++ app/Views/client/dashboard/video_denuncia.php
@@ -7,6 +7,35 @@
 <?= $this->section('content') ?>
 
 <?php $session = session(); ?>
+<style>
+	.video_denunciante {
+		width: 20%;
+		height: 17%;
+		position: relative;
+		background-color: coral;
+		top: 0;
+		left: 0;
+		z-index: 1;
+		margin: 1%;
+		border-radius: 5%;
+	}
+
+	.video_usuario {
+		width: 100%;
+		height: 90%;
+		position: relative;
+		background-color: lightseagreen;
+
+	}
+	.colgar{
+		position: relative;
+		z-index: 2;
+		top: 92%;
+		height: 5%;
+	}
+
+	
+</style>
 <div class="container-fluid mb-5">
 	<div class="row">
 		<div class="col-12 text-center" style="font-size:10px;">
@@ -16,36 +45,55 @@
 		<div class="col-12 p-0 m-0">
 			<div class="card text-center">
 				<div class="card-body p-0 m-0">
-					<div class="ratio ratio-16x9">
-						<iframe style="min-height:600px;" src="<?= 'https://videodenunciaserver1.fgebc.gob.mx/videollamada?folio=' . $body_data->folio . '&nombre=' . $session->NOMBRE . ' ' . $session->APELLIDO_PATERNO . ' ' . $session->APELLIDO_MATERNO . '&delito=' . $body_data->delito . '&descripcion=' . $body_data->descripcion . '&idioma=' . $body_data->idioma . '&edad=' . $body_data->edad . '&perfil=' . $body_data->perfil . '&sexo=' . $body_data->sexo . '&prioridad=' . $body_data->prioridad . '&sexo_denunciante=' . $body_data->sexo_denunciante ?>" frameborder="0" allowfullscreen allow="camera *;microphone *"></iframe>
+					<div class="ratio ratio-16x9 justify-content-center">
+						<!-- <iframe style="min-height:600px;" src="<?= 'https://videodenunciaserver1.fgebc.gob.mx/videollamada?folio=' . $body_data->folio . '&nombre=' . $session->NOMBRE . ' ' . $session->APELLIDO_PATERNO . ' ' . $session->APELLIDO_MATERNO . '&delito=' . $body_data->delito . '&descripcion=' . $body_data->descripcion . '&idioma=' . $body_data->idioma . '&edad=' . $body_data->edad . '&perfil=' . $body_data->perfil . '&sexo=' . $body_data->sexo . '&prioridad=' . $body_data->prioridad . '&sexo_denunciante=' . $body_data->sexo_denunciante ?>" frameborder="0" allowfullscreen allow="camera *;microphone *"></iframe> -->
+
+						<div id="sc1" class="sc mt-50">
+							<div class="video_denunciante">video del denunciante</div>
+						</div>
+						<div id="sc2" class="sc">
+							<div class="video_usuario">video del mp
+							
+							</div>
+							
+						</div>
+						<div id="sc2" class="colgar col-12 col-sm-4 col-md-4 col-lg-4">
+							<button type="button" class="btn btn-primary btn-md btn-block">
+							<i class="bi bi-telephone-minus-fill"></i>
+							</button>
+
+						</div>
+
 					</div>
 
 				</div>
+
 			</div>
-			
+
 		</div>
+
 		<br>
 		<div class="card col-12 p-0 m-0">
-				<form id="form_archivos_externos" method="post" enctype="multipart/form-data" >
-					<input type="text" class="form-control" id="folio" name="folio" hidden>
-					<input type="text" class="form-control" id="year" name="year" hidden>
-					<input type="text" class="form-control" id="autor" name="autor" hidden>
+			<form id="form_archivos_externos" method="post" enctype="multipart/form-data">
+				<input type="text" class="form-control" id="folio" name="folio" hidden>
+				<input type="text" class="form-control" id="year" name="year" hidden>
+				<input type="text" class="form-control" id="autor" name="autor" hidden>
 
-					<div class="row">
+				<div class="row">
 
-						<div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-3 m-lg-3 m-md-3">
-							<label for="documentoArchivo" class="form-label font-weight-bold" style="font-weight: bold;">Documentos a anexar.</label>
-							<input type="file" class="form-control" id="documentoArchivo" name="documentoArchivo" accept="image/jpeg, image/jpg, image/png, .doc, .pdf">
-							<img id="viewDocumentoArchivo" class="img-fluid m-3" src="" style="max-width:100px;">
+					<div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-3 m-lg-3 m-md-3">
+						<label for="documentoArchivo" class="form-label font-weight-bold" style="font-weight: bold;">Documentos a anexar.</label>
+						<input type="file" class="form-control" id="documentoArchivo" name="documentoArchivo" accept="image/jpeg, image/jpg, image/png, .doc, .pdf">
+						<img id="viewDocumentoArchivo" class="img-fluid m-3" src="" style="max-width:100px;">
 
-						</div>
-						<div class="col-12 col-sm-4 col-md-4 col-lg-4 mb-3">
-							<button type="submit" class="btn-sm btn-primary m-lg-4 m-md-4" style="width: 100%;">Enviar documentos</button>
-						</div>
 					</div>
-				</form>
+					<div class="col-12 col-sm-4 col-md-4 col-lg-4 mb-3">
+						<button type="submit" class="btn-sm btn-primary m-lg-4 m-md-4" style="width: 100%;">Enviar documentos</button>
+					</div>
+				</div>
+			</form>
 
-			</div>
+		</div>
 	</div>
 </div>
 </div>
@@ -70,10 +118,11 @@
 		}
 	});
 
-	document.getElementById('form_archivos_externos').addEventListener('submit', function(evt){
-    evt.preventDefault();
-	crearArchivos();
-})
+	document.getElementById('form_archivos_externos').addEventListener('submit', function(evt) {
+		evt.preventDefault();
+		crearArchivos();
+	})
+
 	function crearArchivos() {
 		var packetData = new FormData();
 		packetData.append("documentoArchivo", $("#documentoArchivo")[0].files[0]);
@@ -82,11 +131,11 @@
 		$.ajax({
 			url: "<?= base_url('/data/create_archivos') ?>",
 			method: "POST",
-                dataType: 'json',
-                contentType: false,
-                data: packetData,
-                processData: false,
-                cache: false,
+			dataType: 'json',
+			contentType: false,
+			data: packetData,
+			processData: false,
+			cache: false,
 			success: function(response) {
 				const archivos = response.archivos;
 				if (response.status == 1) {
@@ -101,13 +150,13 @@
 					document.getElementById('documentoArchivo').value = '';
 					preview.setAttribute('src', '');
 
-				} else if(response.status==0){
+				} else if (response.status == 0) {
 					Swal.fire({
 						icon: 'error',
 						text: 'Los archivos no se pudieron subir',
 						confirmButtonColor: '#bf9b55',
 					});
-				}else if(response.status==2){
+				} else if (response.status == 2) {
 					Swal.fire({
 						icon: 'error',
 						text: 'Debes subir un documento',
@@ -121,8 +170,6 @@
 		});
 
 	}
-
-
 </script>
 
 <?= $this->endSection() ?>
