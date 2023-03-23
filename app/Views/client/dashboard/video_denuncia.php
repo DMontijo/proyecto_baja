e733b4d4aa3c516469019a56f82f6c470fab777b
Otoniel Flores <55592246+abdieloflores@users.noreply.github.com>
2023-03-22 17:44:51 -0700

    Fix - MODIFICAR PERSON A PERSONA A UN COSTADO DE MEDIA FILIACIÓN

--- app/Views/client/dashboard/video_denuncia.php
+++ app/Views/client/dashboard/video_denuncia.php
@@ -40,7 +40,6 @@
 					<div class="ratio ratio-16x9">
 						<iframe style="min-height:600px;" src="<?= 'https://videodenunciaserver1.fgebc.gob.mx/videollamada?folio=' . $body_data->folio . '&nombre=' . $session->NOMBRE . ' ' . $session->APELLIDO_PATERNO . ' ' . $session->APELLIDO_MATERNO . '&delito=' . $body_data->delito . '&descripcion=' . $body_data->descripcion . '&idioma=' . $body_data->idioma . '&edad=' . $body_data->edad . '&perfil=' . $body_data->perfil . '&sexo=' . $body_data->sexo . '&prioridad=' . $body_data->prioridad . '&sexo_denunciante=' . $body_data->sexo_denunciante ?>" frameborder="0" allowfullscreen allow="camera *;microphone *"></iframe>
 					</div>
-
 				</div>
 			</div>
 		</div>
