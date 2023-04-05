6b8f813cd52f516143d617870719a248f21fe7ab
Otoniel Flores <55592246+abdieloflores@users.noreply.github.com>
2023-04-05 13:44:55 -0700

    Fix

--- app/Views/admin/dashboard/bandeja/bandeja_remision.php
+++ app/Views/admin/dashboard/bandeja/bandeja_remision.php
@@ -13,12 +13,17 @@
 			<div class="col-12">
 				<div class="card shadow border-0" style="overflow-x:auto;">
 					<div class="card-body">
-					<div id="loading_general" name="loading_general" class="text-center d-none" style="min-height:50px;">
-									<div class="justify-content-center">
-										<div class="spinner-border text-primary" role="status">
-										</div>
-									</div>
+						<div id="loading_general" name="loading_general" class="text-center d-none" style="min-height:50px;">
+							<div class="justify-content-center">
+								<div class="spinner-border text-primary" role="status">
 								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-12 text-center">
+								<a class="btn btn-primary" target="_blank" href="http://172.16.24.161/remision/consulta/" role="button">RECOMENDACIONES REMISIÓN</a>
+							</div>
+						</div>
 						<form id="form_remision" class="row needs-validation" action="<?= base_url() ?>/admin/dashboard/bandeja_remision" method="POST" novalidate>
 							<input autocomplete="off" type="text" name="expediente" class="form-control" id="expediente" value="<?= $body_data->expedienteid ?>" hidden required>
 							<input autocomplete="off" type="text" name="municipio" class="form-control" id="municipio" value="<?= $body_data->municipio ?>" hidden required>
