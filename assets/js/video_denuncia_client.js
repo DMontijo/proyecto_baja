87b542d4678aacb0d7d127e1410ceb1fb109a6b9
Otoniel Flores <55592246+abdieloflores@users.noreply.github.com>
2023-04-07 13:40:38 -0700

    Fix

--- assets/js/video_denuncia_client.js
+++ assets/js/video_denuncia_client.js
@@ -99,9 +99,10 @@ guestVideoService.registerVideoRecordingStatus(isRecording => {
 
 guestVideoService.saveGeolocation(() => {
 	texto_inicial.style.display = "block";
-	setTimeout(() => {
-		guestVideoService.connectGuest({ delito, folio: folio_SY + "/" + year_SF, descripcion }, guest => { console.log(guest); });
-	}, 2000);
+	guestVideoService.connectGuest({ delito, folio: folio_SY + "/" + year_SF, descripcion }, guest => { console.log(guest); });
+	// setTimeout(() => {
+	// 	guestVideoService.connectGuest({ delito, folio: folio_SY + "/" + year_SF, descripcion }, guest => { console.log(guest); });
+	// }, 2000);
 });
 
 function deleteVideoElement() {
