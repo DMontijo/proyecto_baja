d0b5057c195044c3cc256d9be682a56edddd7eff
Otoniel Flores <55592246+abdieloflores@users.noreply.github.com>
2023-04-07 09:31:08 -0700

    Fix

--- app/Views/admin/dashboard/folios/index.php
+++ app/Views/admin/dashboard/folios/index.php
@@ -13,7 +13,6 @@
 			</div>
 			<div class="col-12 col-sm-6 col-md-4 col-lg-3">
 				<div class="card shadow border-0 text-center">
-					<img class="card-img-top" src="holder.js/100px180/" alt="">
 					<div class="card-body p-2" style="height:200px;">
 						<a href="<?= base_url('admin/dashboard/folios_expediente') ?>" class="btn btn-primary btn-block h-100 d-flex flex-column justify-content-center align-items-center">
 							<i class="fas fa-file-alt"></i> Expedientes <br><br> <span class="font-weight-bold" style="font-size:20px;"><?= $body_data->expedientes ?></span>
@@ -23,7 +22,6 @@
 			</div>
 			<div class="col-12 col-sm-6 col-md-4 col-lg-3">
 				<div class="card shadow border-0 text-center">
-					<img class="card-img-top" src="holder.js/100px180/" alt="">
 					<div class="card-body p-2" style="height:200px;">
 						<a href="<?= base_url('admin/dashboard/folios_abiertos') ?>" class="btn btn-primary btn-block h-100 d-flex flex-column justify-content-center align-items-center">
 							<i class="fas fa-box-open"></i> Abiertos <br><br> <span class="font-weight-bold" style="font-size:20px;"><?= $body_data->abiertos ?></span>
@@ -34,7 +32,6 @@
 			</div>
 			<div class="col-12 col-sm-6 col-md-4 col-lg-3">
 				<div class="card shadow border-0 text-center">
-					<img class="card-img-top" src="holder.js/100px180/" alt="">
 					<div class="card-body p-2" style="height:200px;">
 						<a href="<?= base_url('admin/dashboard/folios_derivados') ?>" class="btn btn-primary btn-block h-100 d-flex flex-column justify-content-center align-items-center">
 							<i class="fas fa-share"></i> Derivados <br><br> <span class="font-weight-bold" style="font-size:20px;"><?= $body_data->derivados ?></span>
@@ -44,7 +41,6 @@
 			</div>
 			<div class="col-12 col-sm-6 col-md-4 col-lg-3">
 				<div class="card shadow border-0 text-center">
-					<img class="card-img-top" src="holder.js/100px180/" alt="">
 					<div class="card-body p-2" style="height:200px;">
 						<a href="<?= base_url('admin/dashboard/folios_canalizados') ?>" class="btn btn-primary btn-block h-100 d-flex flex-column justify-content-center align-items-center">
 							<i class="fas fa-share"></i> Canalizados <br><br> <span class="font-weight-bold" style="font-size:20px;"><?= $body_data->canalizados ?></span>
@@ -55,7 +51,6 @@
 			<?php if (in_array(session('ROLID'), $rolesToMonitor)) { ?>
 				<div class="col-12 col-sm-6 col-md-4 col-lg-3">
 					<div class="card shadow border-0 text-center">
-						<img class="card-img-top" src="holder.js/100px180/" alt="">
 						<div class="card-body p-2" style="height:200px;">
 							<a href="<?= base_url('admin/dashboard/folios_en_proceso') ?>" class="btn btn-primary btn-block h-100 d-flex flex-column justify-content-center align-items-center">
 								<i class="fas fa-user-cog"></i> En proceso <br><br> <span class="font-weight-bold" style="font-size:20px;"><?= $body_data->proceso ?></span>
