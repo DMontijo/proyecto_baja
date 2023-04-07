f6457f9e236fb2e0bf4fd0f91f984b8fe0255486
Otoniel Flores <55592246+abdieloflores@users.noreply.github.com>
2023-04-07 09:37:31 -0700

    Fix

--- app/Views/client/templates/dashboard_template.php
+++ app/Views/client/templates/dashboard_template.php
@@ -26,8 +26,8 @@
 	<!-- Mapas -->
 
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8Y8sKd0VSyZcl9kPdCewI2mpXh95AJ-8&callback=initMap&v=weekly" defer></script>
-	<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZnoURjO4MKsTx6_iRb1stAdXiGHLKSrQ&callback=initMap&v=weekly" defer></script> -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8Y8sKd0VSyZcl9kPdCewI2mpXh95AJ-8&callback=initMap2&v=weekly" defer></script>
+	<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZnoURjO4MKsTx6_iRb1stAdXiGHLKSrQ&callback=initMap2&v=weekly" defer></script> -->
 
 
 	<script src="<?= base_url() ?>/assets/guest/guest.js" type="module"></script>
@@ -112,5 +112,8 @@
 		</script>
 	<?php endif; ?>
 </body>
+<script>
+	function initMap2() {}
+</script>
 
 </html>
