5b4aa540bef2558ffd803433fdcbc5d4413d14e1
Otoniel Flores <55592246+abdieloflores@users.noreply.github.com>
2023-04-10 09:43:47 -0700

    Fix - Vista de cola

--- assets/js/index_activos.js
+++ assets/js/index_activos.js
@@ -1,22 +1,23 @@
 import { RoomsSockets } from "../agent/extras.js";
-import { variables } from './variables.js';
+import { variables } from "./variables.js";
 import { PriorityLinesSockets } from "../agent/extras.js";
 
 const { apiKey, apiURI } = variables;
 
-const roomsSockets = new RoomsSockets( { apiURI, apiKey });
+const roomsSockets = new RoomsSockets({ apiURI, apiKey });
 
 const priorityLinesSockets = new PriorityLinesSockets({ apiURI, apiKey });
 
-roomsSockets.registerToRoomsUpdate((response)=>{
-
+roomsSockets.registerToRoomsUpdate(response => {
 	const countUser = response.filter(i => i).length;
 
-	document.getElementById('card_active_users').innerHTML = countUser;
-
-})
-priorityLinesSockets.registerToPriorityLineUpdate(priorityLists => {
-	const countCola = priorityLists.filter(i => i).length;
-	document.getElementById('lista_prioridad_users').innerHTML = countCola;
+	document.getElementById("card_active_users").innerHTML = countUser;
 });
 
+priorityLinesSockets.registerToPriorityLineUpdate(priorityLists => {
+	let countCola = 0;
+	priorityLists.forEach(element => {
+		countCola = countCola + element.guests.length;
+	});
+	document.getElementById("lista_prioridad_users").innerHTML = countCola;
+});
