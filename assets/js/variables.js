246c4270f52b011c8c7d9af9720dbd0a36b788ab
zeika-coder <andygtz99@gmail.com>
2023-04-21 17:03:35 -0600

    ngrok

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -1,5 +1,5 @@
 export const variables = {
-	apiURI: 'https://344c-2806-2f0-5021-4716-a30a-2e44-4dd-66d7.ngrok-free.app',
+	apiURI: 'https://3782-2806-2f0-5021-4716-a30a-2e44-4dd-66d7.ngrok-free.app',
 	// apiURI: 'https://videodenunciabalancer.fgebc.gob.mx',
 	apiKey: 'vspk_6258d819-105e-4487-b7f1-be72e892850e'
  	}
