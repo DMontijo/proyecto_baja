574ae7a002485294d12de2acd9ac220a46461d74
Otoniel Flores <55592246+abdieloflores@users.noreply.github.com>
2023-05-15 16:29:30 -0700

    Fix

--- app/Controllers/extravio/ExtravioController.php
+++ app/Controllers/extravio/ExtravioController.php
@@ -284,6 +284,8 @@ class ExtravioController extends BaseController
 	private function _sendEmailPassword($to, $password)
 	{
 		$user = $this->_denunciantesModelRead->asObject()->where('CORREO', $to)->first();
+		var_dump($user);
+		exit;
 
 		$body = view('email_template/password_email_constancia.php', ['email' => $to, 'password' => $password]);
 		$mailersend = new MailerSend(['api_key' => EMAIL_TOKEN]);
