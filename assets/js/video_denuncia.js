5eb52731eeab9a93914de21556df627973479efb
zeika-coder <andygtz99@gmail.com>
2023-06-25 17:12:49 -0600

    fix boton media configuration

--- assets/js/video_denuncia.js
+++ assets/js/video_denuncia.js
@@ -255,7 +255,7 @@ aceptar_llamada.addEventListener("click", () => {
 			denunciante_nombre_llamada.innerHTML = guest.name;
 			disponible_connect.hidden = true;
 			no_disponible_connect.hidden = true;
-			$mediaConfiguration.hidden = true;
+			$mediaConfiguration.hidden = false;
 			header_llamda.hidden = false;
 			$("#llamadaModal").modal("hide");
 			aceptar_llamada.disabled = false;
