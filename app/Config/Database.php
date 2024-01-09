d5be45b200c8a9a9e91879fa3ca907a2cba14f16
zeika-coder <andygtz99@gmail.com>
2024-01-09 14:48:49 -0600

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -86,10 +86,10 @@ class Database extends Config
 
 	public $development = [
 		'DSN'      => '',
-		'hostname' => 'database-video-denuncia-baja-california-dev-cluster.cluster-cenwfxggsegs.us-east-1.rds.amazonaws.com',
+		'hostname' => 'database-video-denuncia-baja-california-instance-1.cenwfxggsegs.us-east-1.rds.amazonaws.com',
 		'username' => 'fgebc_admin',
 		'password' => 'fgebc_dba0wner',
-		'database' => 'litigantes',
+		'database' => 'cdtec',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
@@ -113,10 +113,10 @@ class Database extends Config
 
 	public $development_read = [
 		'DSN'      => '',
-		'hostname' => 'database-video-denuncia-baja-california-dev-cluster.cluster-ro-cenwfxggsegs.us-east-1.rds.amazonaws.com',
+		'hostname' => 'database-video-denuncia-baja-california-instance-1-us-east-1b.cenwfxggsegs.us-east-1.rds.amazonaws.com',
 		'username' => 'fgebc_admin',
 		'password' => 'fgebc_dba0wner',
-		'database' => 'litigantes',
+		'database' => 'cdtec',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
