332afc94e4784621b6d034388e56d88e8d58d39d
zeika-coder <48071891+zeika-coder@users.noreply.github.com>
2024-04-03 16:22:55 -0600

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -186,33 +186,6 @@ class Database extends Config
 		'port'     => 3306,
 	];
 
-	/**
-	 * This database connection is used when generate reports
-	 * Only read
-	 * @var array
-	 */
-
-	public $reporte_read = [
-		'DSN'      => '',
-		'hostname' => 'database-video-denuncia-baja-california-instance-3-us-east-1a.cenwfxggsegs.us-east-1.rds.amazonaws.com',
-		'username' => 'fgebc_admin',
-		'password' => 'fgebc_dba0wner',
-		'database' => 'cdtec',
-		'DBDriver' => 'MySQLi',
-		'DBPrefix' => '',
-		'pConnect' => false,
-		'DBDebug'  => (ENVIRONMENT !== 'production'),
-		'charset'  => 'utf8',
-		'DBCollat' => 'utf8_general_ci',
-		'swapPre'  => '',
-		'encrypt'  => false,
-		'compress' => false,
-		'strictOn' => false,
-		'failover' => [],
-		'port'     => 3306,
-	];
-
-
 
 
 	/**
